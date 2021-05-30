d8da43cba299be9864fadc04168796463ff5df2c
ar1j1tm94 <am1250@exeter.ac.uk>
2021-05-30 14:00:29 +0530

    Compiler freeze resolved, docs updated

--- README.md
+++ README.md
@@ -2,50 +2,27 @@
 
 ## Description
 
-### This script is designed to read tweets filterted by hashtags and create a csv file. As of this implementation the tweet fields saved in csv file are
-
-* tweet id
-
-* timestamp for when it was created
-
-* full text
-
-* screen name
-
-* location
-
-* coordinates
-
-* hashtags in the tweet
-
-* user follow count
-
-## How it works?
-
-#### At first the script will try read a maximum of 30k tweets for the hastags. It goes as far back as twitter api allows to read. Then it will sleep for *SLEEP_TIME* in **.env** and read *ITEM_LIMIT* tweets when it wakes up. This will repeat for 1400 times unles it is explicitly set higher or lower
-
-#### I wrote this script to delpoy it on a free-tier ec2 and keep it running until I am satisified with the number of tweets collected
+### This is a simple script to fetch tweets unsing Twitter API based on hashtag_phrase. The hashtags can be added in the .env file including fields for API keys. This script writes all the data fetched in .csv file. It does not query historical data.
 
 ## How do I run it?
 
-### To run the script
+1. Add twitter API keys and hashtags in the **.env** file
 
-1. replace your authenitcation details in the **.env** file and the hashtags that interests you
-
-2. install dependencies from requirments.txt
+2. Install dependencies from requirments.txt
 
 ```shell
-python3 -m pip install -r requirements.txt 
+> python3 -m pip install -r requirements.txt 
 ```
 
 3. then as simple as running the script
 
 ```shell
-python3 tweet_scaper.py 
+> python3 tweet_scraper.py 
 ```
 
-### if you are deploying it on a remote machine and execution stops when ssh is disconnected, use this
+4. To run it in detached shell [OPTIONAL]
 
 ```shell
-nohup python3 tweet_scaper.py &
+> nohup python3 tweet_scaper.py &
 ```
+
--- tweet_scraper.py
+++ tweet_scraper.py
@@ -1,30 +1,21 @@
-'''
-This script collects tweets
-'''
+"""This is a simple script to fetch tweets unsing Twitter API based on hashtag_phrase
+The hashtags can be added in the .env file including fields for API keys. This script
+writes all the data fetched in .csv file. 
 
-import os
+Returns:
+    .csv: Fteched tweets are written in this file.
+"""
 import tweepy
+import os
 import csv
-import logging
+import pandas as pd
 import time
-
-from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
-logging.basicConfig(
-    filename='twitter_scraping.log', 
-    level=logging.INFO,
-    filemode='a', 
-    format= '%(asctime)s %(levelname)s: %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
+class Twitter:
 
-class ScrapeScrape():
-
-    def __init__(self, scarpe_size: int):
-        """[summary]
-        """    
+    def __init__(self, scarpe_size):
         self.consumer_key = os.environ.get("CONSUMER_KEY")
         self.consumer_secret = os.environ.get("CONSUMER_SECRET")
         self.access_token = os.environ.get("ACCESS_TOKEN")
@@ -32,75 +23,67 @@ class ScrapeScrape():
         self.hashtag_phrase = os.environ.get("HASHTAG_PHRASE")
         self.fname = os.environ.get("FILE_NAME")
         self.scrape_size = scarpe_size
+        self.header = [
+            'id', 'timestamp', 'tweet_text', 'username', 'location',
+            'coordinates', 'place_json', 'all_hashtags', 'followers_count'
+        ]
 
-    def scrape_tweets(self):
-        """[summary]
-        """    
-        header = ['id', 'timestamp', 'tweet_text', 'username', 'location', 'coordinates', 'place_json', 'all_hashtags', 'followers_count']
-        #create authentication for accessing Twitter
+    def authentication(self) -> tweepy.API:
         auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
         auth.set_access_token(self.access_token, self.access_token_secret)
-
-        #initialize Tweepy API
         api = tweepy.API(auth, wait_on_rate_limit=True)
-        #get the name of the spreadsheet we will write to
-        tweepy_data = tweepy.Cursor(api.search, q=self.hashtag_phrase+' -filter:retweets', lang="en", tweet_mode='extended').items(self.scrape_size)
-        for tweet in reversed(list(tweepy_data)):
+
+        return api
+
+    def get_dataframe(self) -> pd.DataFrame:
+
+        if os.path.isfile(self.fname):
+            print('File exist')
+        else:
+            with open(self.fname, 'a+') as file:
+                w = csv.writer(file)
+                w.writerow(self.header)
+                print('File created')
+
+        df = pd.read_csv('energy_tweets.csv', header=0)
+
+        return df
+
+    def get_tweets(self):
+
+        api = self.authentication()
+        tweepy_data = tweepy.Cursor(api.search, q=self.hashtag_phrase + " -filter:retweets", lang="en",
+                                    tweet_mode='extended').items(self.scrape_size)
+        df = self.get_dataframe()
+        for tweet in tweepy_data:
             row = [
-                tweet.id, 
-                tweet.created_at, 
-                tweet.full_text.replace('\n',' ').encode('utf-8'),
+                tweet.id,
+                tweet.created_at,
+                tweet.full_text.replace('\n', ' ').encode('utf-8'),
                 tweet.user.screen_name.encode('utf-8'),
                 tweet.user.location,
                 tweet.coordinates,
                 tweet.place,
                 [e['text'] for e in tweet._json['entities']['hashtags']],
                 tweet.user.followers_count
-                ]
-            try:
-                with open(self.fname, 'r') as file:
-                    last_id = file.read().splitlines()[-1][0:19]
-                with open(self.fname, 'a+') as file:
-                    if int(row[0]) <= int(last_id):
-                        continue
-                    else:
-                        w = csv.writer(file)
-                        w.writerow(row)
-                        logging.info('New tweet added')
-            except IOError:
-                with open(self.fname, 'w+') as file:
-                    w = csv.writer(file)
-                    w.writerow(header)
-                    w.writerow(row)
-                    logging.info('New tweet added')
-            except Exception as e:
-                logging.error(e)
-                print(e)
-                exit()
-    
+            ]
+            print(row[0], row[1])
+            if row[0] in df['id'].values:
+                continue
+            else:
+                df2 = pd.DataFrame(data=[row], columns=self.header)
+                df = df.append(df2, ignore_index=True)
+        df.sort_values(by=['id'])
+        df.to_csv('energy_tweets.csv', index=False)
+
 
 def main():
-    # Pyhton normaly goes back a few hours from data request.
-    # So I think it is a good idea to do a big scrape at first the do small scrapes
-    # configuration can 30k items at first go and then 3 every two minutes (at least for the hashtags we are using)
-    logging.info('Execution Started: %s', datetime.today())
-
-    big_scraper = ScrapeScrape(30000)
-    big_scraper.scrape_tweets()
-
-    for _ in range(0, 1440):
-        START = time.time()
-        logging.info('Scraping Started: %s', datetime.today())
-        scraper = ScrapeScrape(int(os.getenv('ITEM_LIMIT')))
-        scraper.scrape_tweets()
-        logging.info('Scraping Ended: %s', datetime.today())
-        logging.info('Execution time: %s', (time.time()-START))
-        time.sleep(int(os.getenv('SLEEP_TIME')))
-        logging.info('Woke up to check new tweets')
+    # wakes up every minute collects latest 30 tweets
+    obj = Twitter(30) # Number of new tweets to fetch
+    for _ in range(1441):
+        obj.get_tweets()
+        time.sleep(60)
 
 
 if __name__ == '__main__':
-    
-
     main()
-    logging.critical('Execution Terminated')
